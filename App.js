65fe4552c4dad5dda83ee6101179a22d90843898
Pierre Mesure <pierre.mesure@gmail.com>
2017-10-20 22:06:09 +0200

    Removed MEAN and replaced it by a vanilla React

---
+++ App.js
@@ -0,0 +1,29 @@
+import React from 'react';
+import { StyleSheet, Text, View, Button } from 'react-native';
+
+export default class App extends React.Component {
+  render() {
+    return (
+      <View style={styles.container}>
+        <Text>Hey Xinyi</Text>
+        <Text>Do you want some shrimps?</Text>
+          <Button
+
+              title="Learn More"
+              color="#841584"
+              accessibilityLabel="Learn more about this purple button"
+          />
+        <Text>Shake your phone to open the developer menu.</Text>
+      </View>
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
--- README.md
+++ README.md
@@ -1,1 +1,1 @@
-#testing
+# carpooling
